39c2e7fa5d015a38215a321b3aa4eed2e94bd96c
mikkelvp <mikkelvp@gmail.com>
2016-02-23 23:15:57 +0100

    Fetch GlobalSecurity and retry failed codes.

--- script.js
+++ script.js
@@ -3,19 +3,19 @@
 'use strict';
 
 /* SÆT DATA HER */
-var userId = ''; // COOP medlemsnummer
-var fullName = '';
-var address = '';
-var zip = '';
-var city = '';
-var phone = '';
-var email = '';
-var security = ''; // GlobalSecurity variable genereres af madkonkurrence.dk - console.log(GlobalSecurity);
+var userId = '12345'; // COOP medlemsnummer
+var fullName = 'fuldt navn';
+var address = 'vej 1';
+var zip = '0000';
+var city = 'by';
+var phone = '12345678';
+var email = 'e@mail.com';
 var minWait = 2000; // Min antal millisekunder ventetid mellem koder sendes
 var maxWait = 4000; // Max antal millisekunder
 
 
 ////////////////////////////////////////////////////////////////
+
 var http = require('http');
 var fs = require('fs');
 var codes = fs.readFileSync('koder.txt').toString().split("\n");
@@ -24,14 +24,45 @@ var count = 0;
 var wins = 0;
 var submittedWins = 0;
 var timeToWait = 0;
-var failCount = 0;
+var failedCodes = [];
+var security;
 
 console.log(codes.length + ' koder fundet.');
 
-for (var i in codes) {
-    codes[i] = codes[i].substr(0, 5);
-    setTimeout(checkCode, timeToWait, codes[i]);
-    timeToWait += getRandomInt(minWait, maxWait);
+function start() {
+    for (var i in codes) {
+        codes[i] = codes[i].substr(0, 5);
+        setTimeout(checkCode, timeToWait, codes[i]);
+        timeToWait += getRandomInt(minWait, maxWait);
+    }
+    setInterval(end, 2000);
+}
+
+function getGlobalSecurity(callback) {
+    var data = [];
+    var options = {
+        host: hostname,
+        port: 80,
+        path: '/'
+    };
+    http.get(options, function(res) {
+        res.on("data", function(chunk) {
+            data.push(chunk);
+        });
+
+        res.on('end', function() {
+            var htmlString = data.join('');
+            var indexOfSec = htmlString.indexOf('GlobalSecurity');
+            var sec = htmlString.substr(indexOfSec);
+            var arr = sec.split("'");
+            security = arr[1];
+            callback();
+        });
+    }).on('error', function(err) {
+        console.log('failed to get GlobalSecurity variable');
+        console.log(err);
+        process.exit();
+    });
 }
 
 function getRandomInt(min, max) {
@@ -79,7 +110,7 @@ function checkCode(code) {
     });
 
     req.on('error', function(err) {
-        failCount++;
+        failedCodes.push(code);
         console.log(code + ': Error.');
         console.log(err);
     });
@@ -129,7 +160,15 @@ function submitWinnerInfo(winnerId) {
 }
 
 function end() {
-    if (count + failCount === codes.length && wins === submittedWins) {
+    var _timeToWait = 0;
+    if (failedCodes.length > 0) {
+        console.log(failedCodes.length + ' failed. Retrying.');
+        for (var i in failedCodes) {
+            _timeToWait += getRandomInt(minWait, maxWait);
+            setTimeout(checkCode, _timeToWait, failedCodes.pop());
+        }
+    } else if (count === codes.length && wins === submittedWins) {
+
         var data = [];
         var reqData = JSON.stringify({
             UserId: userId,
@@ -156,15 +195,15 @@ function end() {
         });
 
         req.on('error', function(err) {
+            console.log('ERROR:');
             console.log(err);
         });
         req.write(reqData);
         req.end();
 
-        console.log('Submitted ' + count + ' codes.');
-        console.log(failCount + ' failed.');
+        console.log(count + ' koder sendt.');
         console.log(wins + ' wins.');
     }
 }
 
-setInterval(end, 2000);
+getGlobalSecurity(start);
